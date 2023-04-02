531f0692e49655fb0da6fd3869fbf44a5dcc1977
Daya Darsan <dayadarsan1802@gmail.com>
2023-04-02 20:18:42 +0530

    Add dependencies + ImGui demo rendering

--- ImGui-Standalone-Demo.cpp
+++ ImGui-Standalone-Demo.cpp
@@ -1,7 +1,48 @@
-﻿#include <iostream>
+﻿#include <GLFW/glfw3.h>
+
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	glfwInit ();
+
+	auto window { glfwCreateWindow ( 1280, 720, "ImGui-Standalone-Demo", nullptr, nullptr ) };
+	glfwMakeContextCurrent ( window );
+
+	auto imGuiContext { ImGui::CreateContext () };
+	ImGui::SetCurrentContext ( imGuiContext );
+
+	ImGui_ImplGlfw_InitForOpenGL ( window, true );
+	ImGui_ImplOpenGL3_Init ();
+
+	while ( ! glfwWindowShouldClose ( window ) )
+	{
+		glfwPollEvents ();
+
+		glClear ( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+		ImGui_ImplGlfw_NewFrame ();
+		ImGui_ImplOpenGL3_NewFrame ();
+		
+		ImGui::NewFrame ();
+
+		ImGui::ShowDemoWindow ();
+
+		ImGui::EndFrame ();
+
+		ImGui::Render ();
+		ImGui_ImplOpenGL3_RenderDrawData ( ImGui::GetDrawData () );
+		
+		glfwSwapBuffers ( window );
+	}
+
+	ImGui_ImplOpenGL3_Shutdown ();
+	ImGui_ImplGlfw_Shutdown ();
+	ImGui::DestroyContext ( imGuiContext );
+	glfwDestroyWindow ( window );
+	glfwTerminate ();
+
 	return 0;
 }
